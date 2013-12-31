85e144295492a462df190d19a7c0d06bd691ae8f
maximuszeng <maximuszeng@gmail.com>
2013-12-31 17:31:38 +0800

    Temp commited.

---
+++ src/main/org/maximus/learning/springinaction/essentials/ch2/bf/TestBeanFactory.java
@@ -0,0 +1,15 @@
+package org.maximus.learning.springinaction.essentials.ch2.bf;
+
+import java.io.FileInputStream;
+
+public class TestBeanFactory {
+
+	BeanFactory factory =
+		       new XmlBeanFactory(new FileInputStream("beans.xml"));
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
